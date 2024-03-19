9fc148bfc82dd869770408c26f2be91baa50b8ee
MrDionesalvi <dionesalvip@gmail.com>
2024-03-19 19:49:26 +0100

    ultimo set risolto

---
+++ Lab03/5_ContaUno.asm
@@ -0,0 +1,31 @@
+.globl _start
+
+.data 
+	# variables here 
+.text
+
+_start:
+	li t0, 37 # t0 = N = 37
+	li t1, 1 # AND MASK
+	li t2, 0 # Counter
+
+	
+TOP:    ble t0, zero, END
+	and t3, t0, t1
+	bne t3, t1, NOUNO
+	addi t2, t2, 1
+NOUNO:
+	srli t0, t0, 1
+	beq zero, zero, TOP
+END: 
+
+print:
+        addi  a0, t2, 0    	 # print the result
+        li    a7, 1
+        ecall
+	
+
+
+exit:
+    addi x17, x0, 10  # call number 10 = exit
+    ecall
---
+++ Lab03/6_CicliForAnnidati.asm
@@ -0,0 +1,37 @@
+.globl _start
+
+.data 
+	# variables here 
+.text
+
+_start:
+	li t0, 10 # t0 = a
+	li t1, 5 # t1 = b
+	li t2, 0 # t2 = R
+	
+	li t3, 0 # i
+	li t4, 0 # j
+	
+TOP:    bge t3, t0, END
+	li t4, 0 # j
+J:	bge t4, t1, FEND
+	add t5, t2, t2 # 2* R
+	add t2, t5, t3 # R = (2*R) + i
+	add t2, t2, t4 # R = (2*R) + i + j
+	addi t4, t4, 1
+	beq zero, zero, J
+FEND:
+	addi t3, t3, 1
+	beq zero, zero, TOP
+END: 
+
+print:
+        addi  a0, t2, 0    	 # print the result
+        li    a7, 1
+        ecall
+	
+
+
+exit:
+    addi x17, x0, 10  # call number 10 = exit
+    ecall
