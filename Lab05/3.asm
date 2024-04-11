6c1a00d1f6df8b1937cc68cab1b4e461cfb00d16
MrDionesalvi <dionealvip@gmail.com>
2024-04-11 09:41:28 +0200

    Lab05: Esercizio 3

--- Lab05/3.asm
+++ Lab05/3.asm
@@ -15,3 +15,22 @@ _start:
 #******************************************
 # completare la funzione nel campo di sotto
 hash:
+	li t0, 5381 # Hash
+	li t1, 0 # i counter
+	
+TOP:
+	lbu t2, 0(a0)
+	addi a0, a0, 1
+	beq t2, zero, END # While
+	
+	add t3, zero, t0 # Copia di Has in t3
+	slli t3, t3, 5 # hash << 5
+	add t3, t3, t0
+	add t3, t3, t2
+	add t0, zero, t3
+	j TOP
+END:	
+	add a0, zero, t0
+	jr ra
+	
+	
